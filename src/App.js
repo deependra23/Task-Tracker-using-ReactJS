8da4044daa8be224f676437addb739ae5b2078b2
Deependra <deependrapoudel19@gmail.com>
2021-11-02 15:04:33 +1100

    upto listing tasks

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from 'react'
+
+function App() {
+  const [tasks, setTasks] = useState ([
+    {
+        id:1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30PM',
+        reminder: true,
+    },
+    {
+        id:2,
+        text: 'Meeting at School',
+        day: 'Feb 5th at 2:30PM',
+        reminder: true,
+    },
+    {
+        id:3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30PM',
+        reminder: false,
+    },
+
+])
+
+  return (
+    <div className='container'>
+        < Header title="Task Tracker" />
+        <Tasks tasks = {tasks} />
+        
+    </div>
+  );
+}
+
+export default App
